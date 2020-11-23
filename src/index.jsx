2ee87c35cb41bc55049faa1be9ac773a1a7c2409
coxcore <ascript0@gmail.com>
2020-11-23 20:09:40 +0900

    modify to use number type in list props

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const loop = (Item, list, each = (data) => data, instead = null) =>
-    Item && list && list.length > 0
-        ? list.map((data, index) => {
+const getArray = (n) => {
+    if (typeof n === 'number' && n > 0) {
+        const arr = Array(n);
+
+        for (let i = 0; i < n; i++) {
+            arr[i] = i + 1;
+        }
+
+        return arr;
+    }
+
+    return n;
+};
+
+export const loop = (Item, list, each = (data) => data, instead = null) => {
+    const datas = getArray(list);
+
+    return Item && datas && datas.length > 0
+        ? datas.map((data, index) => {
               const props = each(data, index);
               return <Item key={index} {...props} />;
           })
         : instead;
+};
 
 const LoopItem = ({ target, list, each, instead }) =>
     loop(target, list, each, instead);
 
 LoopItem.propTypes = {
     target: PropTypes.elementType || PropTypes.any,
-    list: PropTypes.array,
+    list: PropTypes.oneOfType([PropTypes.array, PropTypes.number]),
     each: PropTypes.func,
     instead: PropTypes.element,
 };
