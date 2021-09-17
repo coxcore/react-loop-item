ffdc81af405c4de49925433ef9834aa400922a1e
ascript <ascript0@gmail.com>
2021-09-17 23:13:03 +0900

    modify development environment

--- index.d.ts
+++ index.d.ts
@@ -1,24 +1,24 @@
 import React from 'react';
 
-type forwardRef = {render: Function};
+type forwardRef = { render: Function };
 
 type Component = Function | string | forwardRef;
-type eachCallback = (data:any, index?:number) => (object | null);
+type eachCallback = (data: any, index?: number) => object | null;
 
 export function loop(
-  Item: Component,
-  list?: Array<any> | number | null,
-  each?: eachCallback | null,
-  instead?: React.ReactNode,
-  hidden?: boolean | any
+    Item: Component,
+    list?: Array<any> | number | null,
+    each?: eachCallback | null,
+    instead?: React.ReactNode,
+    hidden?: boolean | any
 ): React.ReactNode;
 
 export interface LoopItemProps {
-  target: Component;
-  list?: Array<any> | number | null;
-  each?: eachCallback | null;
-  instead?: React.ReactNode;
-  hidden?: boolean | any;
+    target: Component;
+    list?: Array<any> | number | null;
+    each?: eachCallback | null;
+    instead?: React.ReactNode;
+    hidden?: boolean | any;
 }
 
 declare const LoopItem: React.FC<LoopItemProps>;
